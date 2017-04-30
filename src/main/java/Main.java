6d80c2b252209e1ac4d93bcec706afcab5b0698a
dorin95dp <dorin.popa1995@gmail.com>
2017-05-01 01:57:52 +0300

    Create POP3 client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,33 @@
+import org.apache.commons.net.pop3.POP3Client;
+import org.apache.commons.net.pop3.POP3MessageInfo;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main( String[] args ) {
+
+        System.out.println("Client simplu de posta – citire mesaj");
+        try {
+            POP3Client client = new POP3Client();
+            client.connect("127.0.0.1",110);
+            if (client.login("dorin","10")) {
+                POP3MessageInfo[] messages = client.listMessages();
+                System.out.println("Mesaje: " + messages.length);
+                System.out.println("Primul mesaj");
+                Reader r = client.retrieveMessage(messages[0].number);
+                BufferedReader br = new BufferedReader(r);
+                String line;
+                while((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } else {
+                System.out.println("Logare fara succes...");
+            }
+            client.logout();
+            client.disconnect();
+        } catch (IOException ex){
+            System.out.println("Error on connection");
+        }
+
     }
 }
