9b3d46f09cac0fff60d04c7586a10a32aa273cfa
dorin95dp <dorin.popa1995@gmail.com>
2017-05-03 13:17:52 +0300

    Send email to Dragos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,21 @@
-import org.apache.commons.net.pop3.POP3Client;
-import org.apache.commons.net.pop3.POP3MessageInfo;
-import java.io.*;
+import org.apache.commons.mail.*;
 
 public class Main {
 
     public static void main( String[] args ) {
 
-        System.out.println("Client simplu de posta – citire mesaj");
+        System.out.println("Client simplu de posta – trimitere mesaj");
         try {
-            POP3Client client = new POP3Client();
-            client.connect("127.0.0.1",110);
-            if (client.login("dorin","10")) {
-                POP3MessageInfo[] messages = client.listMessages();
-                System.out.println("Mesaje: " + messages.length);
-                System.out.println("Primul mesaj");
-                Reader r = client.retrieveMessage(messages[0].number);
-                BufferedReader br = new BufferedReader(r);
-                String line;
-                while((line = br.readLine()) != null) {
-                    System.out.println(line);
-                }
-            } else {
-                System.out.println("Logare fara succes...");
-            }
-            client.logout();
-            client.disconnect();
-        } catch (IOException ex){
-            System.out.println("Error on connection");
+            Email email = new SimpleEmail();
+            email.setHostName("192.168.55.163");
+            email.setSmtpPort(2525);
+            email.setFrom("dorin@domain1.com");
+            email.setSubject("Subject2");
+            email.setMsg("Message2");
+            email.addTo("dr@domain.com");
+            email.send();
+        } catch (EmailException e) {
+            System.out.println(e.getMessage());
         }
-
     }
 }
