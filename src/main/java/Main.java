5263ee612bb81cfea2be3a9de3bedb8a183cb76c
dorin95dp <dorin.popa1995@gmail.com>
2017-05-08 20:24:01 +0300

    Set maven compiler source and target to 1.8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,61 @@
 import modules.EmailReader;
 import modules.EmailWriter;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main( String[] args ) {
-        boolean reading = true;
-        boolean writing = !reading;
+    public static void main( String[] args ) throws IOException {
+        printMenu();
 
-        if (reading) {
-            System.out.println("Reading message...");
+    }
 
-            EmailReader emailReader = new EmailReader();
-            emailReader.readFrom(0);
+    private static void printMenu () {
+        System.out.println("Menu");
+        System.out.println("1. Write message");
+        System.out.println("2. Read latest message");
+        System.out.println("3. Read specific message");
+        Scanner scanner = new Scanner(System.in);
+        EmailReader emailReader = new EmailReader("vasea", "07");
+        EmailWriter emailWriter = new EmailWriter();
+
+        switch (scanner.nextInt()) {
+            case 1:
+                clearConsole();
+                System.out.println("Destination email: ");
+                String destinationEmail = scanner.next();
+
+                System.out.println("Subject: ");
+                String subject = scanner.nextLine();
+
+                System.out.println("Message: ");
+                String message = scanner.nextLine();
+
+                emailWriter.writeTo(destinationEmail, subject, message);
+
+                break;
+            case 2:
+                emailReader.readMail(0);
+
+                break;
+            case 3:
+                clearConsole();
+                System.out.println("Which message?");
+                emailReader.readMail(scanner.nextInt());
+
+                break;
+            default:
+                System.out.println("Default choice");
+                break;
         }
 
-        if (writing) {
-            System.out.println("Client for sending message");
+        return;
+    }
 
-            EmailWriter emailWriter = new EmailWriter();
-            emailWriter.writeTo("vasea@mail.md",
-                                "subject careva",
-                                "Inca un mesaj pentru Vasea");
-
+    public final static void clearConsole() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("");
         }
-
     }
 }
--- src/main/java/modules/EmailReader.java
+++ src/main/java/modules/EmailReader.java
@@ -8,28 +8,43 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class EmailReader {
-    public void readFrom(int messageNr) {
+
+    private String userName;
+    private String userPassword;
+
+    public EmailReader (String userName, String password) {
+        this.userName = userName;
+        this.userPassword = password;
+    }
+
+    public void readMail(int messageNr) {
         try {
             POP3Client client = new POP3Client();
             client.connect("127.0.0.1", 110);
-            if (client.login("vasea", "07")) {
+            if (client.login(userName, userPassword)) {
                 POP3MessageInfo[] messages = client.listMessages();
-                System.out.println("Mesaje: " + messages.length);
-                System.out.println("Primul mesaj");
+                System.out.println("Total nr of messages: " + messages.length);
+                System.out.println("Message" + messageNr);
+                System.out.println("--------------------------------------------------");
 
                 Reader r = client.retrieveMessage(messages[messageNr].number);
                 BufferedReader br = new BufferedReader(r);
                 String line;
+                int i = 0;
                 while ((line = br.readLine()) != null) {
-                    System.out.println(line);
+                    i++;
+                    if (i == 9 || i >= 13) {
+                        System.out.println(line);
+                    }
                 }
             } else {
                 System.out.println("Logare fara succes...");
             }
-//            client.logout();
-//            client.disconnect();
+            client.logout();
+            client.disconnect();
         } catch (IOException ex) {
             System.out.println("Error on connection");
         }
     }
+
 }
