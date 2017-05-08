29dde772778a1d0c32cce1ba648c141b3f9d92e2
dorin95dp <dorin.popa1995@gmail.com>
2017-05-08 13:05:23 +0300

    Send email to vasea

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,12 @@ public class Main {
         System.out.println("Client simplu de posta – trimitere mesaj");
         try {
             Email email = new SimpleEmail();
-            email.setHostName("192.168.55.163");
-            email.setSmtpPort(2525);
-            email.setFrom("dorin@domain1.com");
-            email.setSubject("Subject2");
-            email.setMsg("Message2");
-            email.addTo("dr@domain.com");
+            email.setHostName("127.0.0.1");
+            email.setSmtpPort(25);
+            email.setFrom("dorin@mail.md");
+            email.setSubject("Subject3");
+            email.setMsg("Message 3 - bla bla bla");
+            email.addTo("vasea@mail.md");
             email.send();
         } catch (EmailException e) {
             System.out.println(e.getMessage());
